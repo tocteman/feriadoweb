366b30f85e16e0798b2584972a1b6a4d22caac40
tocteman <juanfrancisco.vinueza@gmail.com>
2022-10-16 18:20:01 -0500

    cambio nro whatsapp

--- src/lib/ui/Whatsapp.svelte
+++ src/lib/ui/Whatsapp.svelte
@@ -3,7 +3,7 @@
 </script>
   <div class="flex justify-end text-right">
     <div class="mr-4 pb-2">
-      <a href="https://api.whatsapp.com/send?phone=+593979391231" >
+      <a href="https://api.whatsapp.com/send?phone=+593988528400" >
         <img src={whatsapp} alt="" class="w-12 cursor-pointer">
       </a>
     </div>
