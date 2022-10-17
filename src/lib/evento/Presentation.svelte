38889b9d5f0372e3359ae8da93a5d2012cd3e75a
tocteman <juanfrancisco.vinueza@gmail.com>
2022-10-17 11:44:01 -0500

    bump

--- src/lib/evento/Presentation.svelte
+++ src/lib/evento/Presentation.svelte
@@ -3,17 +3,12 @@
 </script>
 
 <div>
-  <!-- <h2 class="text-2xl font-display text-lime-400"> -->
-  <!--   28 de octubre -->
-  <!-- </h2> -->
-  <!-- <h2 class="text-2xl font-display text-yellow-300"> -->
-  <!--   caballo campana -->
-  <!-- </h2> -->
   <div class="mx-auto my-4">
     <img src={illustrations.viernes28} alt="" class="-ml-4">
   </div>
   <p class="text-xl my-4">
       ¡Regresa el Campanazo 🎃 y su fiesta de Halloween 🧛🧛‍♀️ para todas las edades!
   </p>
-
+  <p class="text-xl my-4">
+    Te traemos grandes sorpresas y una ambientación GLOW NEON 🌈 . Ven disfrazado 👨🚀 y gana premios, órdenes de consumo y más 💰 </p>
 </div>
